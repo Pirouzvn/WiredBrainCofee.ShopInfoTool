43abdabef509ddd64cc36a7abc1212e8ac82e7e5
DESKTOP-RAH8H8L\Mansour.VN <pirouz.vn@gmail.com>
2020-10-05 14:03:58 +0330

    add project

---
+++ WireBrainCoffee.ShopInfoTool/WireBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WireBrainCoffee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HWired Brain CoffEE - Shop Info Tool!");
+        }
+    }
+}
