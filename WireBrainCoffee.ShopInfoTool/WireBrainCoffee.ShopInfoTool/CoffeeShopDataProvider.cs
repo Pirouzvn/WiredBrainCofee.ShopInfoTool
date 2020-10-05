05b140d2b96d0e08fb54fba846ba5442737d8ed6
DESKTOP-RAH8H8L\Mansour.VN <pirouz.vn@gmail.com>
2020-10-05 16:12:35 +0330

    Add data access project to solution

---
+++ WireBrainCoffee.ShopInfoTool/WireBrainCoffee.ShopInfoTool/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WireBrainCoffee.ShopInfoTool
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShop()
+        {
+            yield return new CoffeeShop { location = "Frankfurt", BeansInStocking = 107 };
+            yield return new CoffeeShop { location = "Freiburg", BeansInStocking = 23 };
+            yield return new CoffeeShop { location = "Munich", BeansInStocking = 56 };
+        }
+    }
+}
